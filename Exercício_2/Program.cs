52e280e55ff3c0303e71e94fa59bb90cb8a49e34
LuanP.Castro <luan.castro@viannasempre.com.br>
2024-02-22 13:42:06 -0300

    Exercício 2 - Converter quantidade em dólar para reais

---
+++ Exercício_2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Faça um programa que:
+// - Leia a cotação do dólar
+// - Leia um valor em dólares
+// - Converta esse valor para Real
+// - Mostre o resultado
+
+
+double cotacaodolar;
+double quantidadedolar;
+double valoremreais;
+
+Console.WriteLine("Informe a cotação do dólar: ");
+cotacaodolar = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Informe o valor em dólar que você possui: ");
+quantidadedolar = Convert.ToDouble(Console.ReadLine());
+
+valoremreais = quantidadedolar / cotacaodolar;
+
+Console.WriteLine("Você possui o equivalente a: R$ {0:N2} reais", valoremreais);
