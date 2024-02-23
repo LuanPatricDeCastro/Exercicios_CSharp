d93443f53876954703729de5454618b92b5892b1
LuanP.Castro <luan.castro@viannasempre.com.br>
2024-02-23 10:25:00 -0300

    Exercício 7 - Transformando Fahrenheit em Celsius

---
+++ Exercício_7/Program.cs
@@ -0,0 +1,18 @@
+﻿// Ler uma temperatura em graus Fahrenheit e apresentá-Ia convertida em graus Celsius. A fórmula de conversão de
+// temperatura a ser utilizada é C = (F - 32) * 5 / 9, em que a variável F é a temperatura em graus Fahrenheit e a
+// variável C é a temperatura em graus Celsius.
+
+double fahrenheit, celsius;
+
+Console.WriteLine("Informe a temperatura em graus Fahrenheit: ");
+fahrenheit = Convert.ToDouble(Console.ReadLine());
+if(fahrenheit < -459.67)
+{
+    Console.WriteLine("Favor verificar a temperatura, pois ela corresponde a uma temperatura inferior ao zero absoluto");
+    Console.WriteLine("Informe a temperatura em graus Fahrenheit novamente: ");
+    fahrenheit = Convert.ToDouble(Console.ReadLine());
+}
+
+celsius = ((fahrenheit - 32) * 5) / 9;
+
+Console.WriteLine("A temperatura informada corresponde a: {0:n2} ºC ", celsius);
