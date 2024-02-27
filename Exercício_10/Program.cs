43c317fd85309e6704a926356147d3ecb754eaad
LuanP.Castro <luan.castro@viannasempre.com.br>
2024-02-27 19:33:42 -0300

    Exercício 10 - Comparação de dois números inteiros (resolução)

--- Exercício_10/Program.cs
+++ Exercício_10/Program.cs
@@ -1,9 +1,6 @@
 ﻿// Escrever um programa que leia dois números inteiros e mostre todos os relacionamentos de ordem existentes
 // entre eles. Os relacionamentos possíveis são: Igual, Não igual, Maior, Menor, Maior ou igual, Menor ou igual.
 
-using System.Collections;
-using System.Linq.Expressions;
-
 int a, b;
 
 Console.Write("Informe o primeiro número: ");
@@ -11,17 +8,21 @@ a = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Informe o segundo número: ");
 b = Convert.ToInt32(Console.ReadLine());
+
 if(a > b)
 {
     Console.WriteLine("O primeiro número é maior.");
 } else if (b > a)
 {
     Console.WriteLine("O segundo número informado é maior.");
-} else if(a != b)
+} else if(a == b)
 {
-    Console.WriteLine("Os números informados são diferentes");
-} else if ()
+    Console.WriteLine("Os números informados são iguais");
+} else if (a >= b)
 {
-
+    Console.WriteLine("O número {0} é maior ou igual do que o número {1}", a , b);
+} else if(a <= b)
+{
+    Console.WriteLine("O número {0} é menor ou igual do que o número {1}", a , b);
 }
 
