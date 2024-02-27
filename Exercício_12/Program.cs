24666f037131d4124b5d8d7f0e23f62e35089843
LuanP.Castro <luan.castro@viannasempre.com.br>
2024-02-27 20:14:29 -0300

    Exercício 12 - Calculando o valor do módulo de X

---
+++ Exercício_12/Program.cs
@@ -0,0 +1,19 @@
+﻿// Escreva um programa que leia um número inteiro e exiba o seu módulo.
+// O módulo de um número x é:
+// x se x é maior ou igual a zero
+// x * (-1) se x é menor que zero
+
+int x;
+
+Console.Write("Informe o valor de x: ");
+x = Convert.ToInt32(Console.ReadLine());
+
+if(x >= 0)
+{
+    Console.WriteLine("O valor do módulo de X é: {0}", x);
+}
+else
+{
+    x = x *(-1);
+    Console.WriteLine("O valor do módulo de x é: {0}", x);   
+}
