0c744b7cc5f09ccebbfc901891ca04e9bd33da8a
LuanP.Castro <luan.castro@viannasempre.com.br>
2024-03-02 18:20:43 -0300

    Exercício 14 - Ler dois números e apresentar a diferença entre eles

---
+++ Exercício_14/Program.cs
@@ -0,0 +1,20 @@
+﻿// Escreva um programa que leia dois números e apresente a diferença do maior para o menor.
+
+double valora, valorb, diferenca;
+
+Console.Write("Informe o primeiro valor: ");
+valora = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Informe o segundo valor: ");
+valorb = Convert.ToDouble(Console.ReadLine());
+
+if(valora >= valorb)
+{
+    diferenca = valora - valorb;
+    Console.WriteLine("A diferença entre os dois valores é de: {0}", diferenca);
+}
+else 
+{
+    diferenca = valorb - valora;
+    Console.WriteLine("A diferença entre os dois valores é de: {0}", diferenca);
+}
