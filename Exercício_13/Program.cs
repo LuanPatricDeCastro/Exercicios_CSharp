85e38df23baeb50f0b9df2b8b8968df9d38a4fe7
LuanP.Castro <luan.castro@viannasempre.com.br>
2024-03-02 16:29:05 -0300

    Exercício 13 - Ler 3 números e imprimir os valores em ordem decrescente

---
+++ Exercício_13/Program.cs
@@ -0,0 +1,39 @@
+﻿// Escreva um programa que leia 3 números inteiros e imprima na tela os valores em ordem decrescente.
+
+int valora, valorb, valorc, maiorvalor, valorintermediario, menorvalor;
+
+Console.Write("Insira o primeiro número inteiro: ");
+valora = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Insira o segundo número inteiro: ");
+valorb = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Insira o terceiro número inteiro: ");
+valorc = Convert.ToInt32(Console.ReadLine());
+
+maiorvalor = valora;
+valorintermediario = 0;
+menorvalor = 0;
+
+if (valora >= valorb && valora >= valorc && valorb >= valorc)
+{
+    maiorvalor = valora;
+    valorintermediario = valorb;
+    menorvalor = valorc;
+}
+else if (valorb >= valora && valorb >= valorc && valora >= valorc)
+{
+    maiorvalor = valorb;
+    valorintermediario = valora;
+    menorvalor = valorc;
+}
+else if (valorc >= valora && valorc >= valorb && valorb >= valora)
+{
+    maiorvalor = valorc;
+    valorintermediario = valorb;
+    menorvalor = valora;
+}
+
+Console.Write("Os valores digitados em ordem decrescente foram: {0} , {1}, {2}", maiorvalor, valorintermediario, menorvalor);
+
+
