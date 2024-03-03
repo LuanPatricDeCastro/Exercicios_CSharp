e4ac20223ec0b357515dfa009c4ada9673b75026
LuanP.Castro <luan.castro@viannasempre.com.br>
2024-03-03 06:09:11 -0300

    Exercício 17 - Valor válido e valor inválido

---
+++ Exercício_17/Program.cs
@@ -0,0 +1,17 @@
+﻿// Escreva um programa que leia um número inteiro. Verificar por meio de condição se o valor fornecido está na faixa
+// entre 0 (zero) e 9 (nove). Caso o valor fornecido esteja dentro da faixa, apresentar a mensagem “valor válido”.
+// Caso contrário, apresentar a mensagem “valor inválido”.
+
+
+int valor;
+
+Console.Write("Informe um valor inteiro: ");
+valor = Convert.ToInt32(Console.ReadLine());
+if(valor >= 0 && valor <= 9)
+{
+    Console.WriteLine("valor válido.");
+}
+else
+{
+    Console.WriteLine("valor inválido");
+}
