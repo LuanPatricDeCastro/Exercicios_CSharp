ee152ece7734371f1de4e2fa93928951eb8934c9
LuanP.Castro <luan.castro@viannasempre.com.br>
2024-03-05 21:11:34 -0300

    Exercício 20 - multiplicação e divisão de valor maior e menor

---
+++ Exercício_20/Program.cs
@@ -0,0 +1,56 @@
+﻿// Escrever um programa declarando três variáveis do tipo inteiro (a, b e c). Ler um valor maior que zero para cada
+// variável (se o valor digitado não é válido, mostrar mensagem e ler novamente). Exibe o menor valor lido
+// multiplicado pelo maior e o maior valor dividido pelo menor.
+
+
+int a, b, c, menorvalor, maiorvalor, multiplicar, dividir;
+
+Console.Write("Informe o valor de a: ");
+a = Convert.ToInt32(Console.ReadLine());
+if(a < 0)
+{
+    Console.WriteLine("Informe o valor novamente.");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write("Informe o valor de b: ");
+b = Convert.ToInt32(Console.ReadLine());
+if(b < 0)
+{
+    Console.WriteLine("Informe o valor novamente.");
+    b = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.Write("Informe o valor de c: ");
+c = Convert.ToInt32(Console.ReadLine());
+if(c < 0)
+{
+    Console.WriteLine("Informe o valor novamente.");
+    c = Convert.ToInt32(Console.ReadLine());
+}
+
+menorvalor = a;
+if(b < a)
+{
+    menorvalor = b;
+}
+else if(c < a)
+{
+    menorvalor = c; 
+}
+
+maiorvalor = a;
+if(b > a)
+{
+    maiorvalor = b;
+}
+else if(c > a)
+{
+    maiorvalor = c;
+}
+
+multiplicar = menorvalor * maiorvalor;
+dividir = maiorvalor / menorvalor;
+
+Console.Write("O resultado da multiplicação é: {0}", multiplicar);
+Console.Write("O resultado da divisão é: {0}", dividir);
