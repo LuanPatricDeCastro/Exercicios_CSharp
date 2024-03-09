af634557eddac96059e683b536f0cd9941e049b6
LuanP.Castro <luan.castro@viannasempre.com.br>
2024-03-09 15:59:23 -0300

    Exercício 22 - Ler um número e caso positivo armazenar em A ou em B

---
+++ Exercício_22/Program.cs
@@ -0,0 +1,23 @@
+﻿// Faça um programa que leia um número. Se positivo armazene-o em A, se for negativo, em B. No final mostrar o
+// resultado.
+
+
+double numero;
+
+Console.Write("Informe um numero: ");
+numero = Convert.ToDouble(Console.ReadLine());
+
+double A, B;
+if(numero > 0)
+{
+    A = 0;
+    A = numero;
+    Console.WriteLine("o número foi armazenado na variável A.");
+    Console.WriteLine(A);
+}else
+{
+    B = 0;
+    B = numero;
+    Console.WriteLine("o número foi armazenado na variável B.");
+    Console.WriteLine(B);
+}
