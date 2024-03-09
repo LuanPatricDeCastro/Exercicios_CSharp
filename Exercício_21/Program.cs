9f7cd67bcb6373e73de99dd9e414b7b1557e76aa
LuanP.Castro <luan.castro@viannasempre.com.br>
2024-03-09 15:08:07 -0300

    Exercício 21 - Ler um número e informar se é positivo ou negativo

---
+++ Exercício_21/Program.cs
@@ -0,0 +1,16 @@
+﻿// Faça um programa que leia um número inteiro e mostre uma mensagem na tela indicando se este número é
+// positivo ou negativo. Pare a execução do programa quando o usuário requisitar.
+
+int numero;
+
+Console.Write("Informe um numero inteiro: ");
+numero = Convert.ToInt32(Console.ReadLine());
+switch (numero)
+{
+    case > 0:
+        Console.WriteLine("O número informado é positivo");
+        break;
+    case < 0:
+        Console.WriteLine("O número informado é negativo.");
+        break;
+}
