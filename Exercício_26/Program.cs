7a57c93ca1abea38230aa7d83b549d76f1b94378
LuanP.Castro <luan.castro@viannasempre.com.br>
2024-04-25 09:36:53 -0300

    Exercício 26 - Imprimindo números de 100 até 1

---
+++ Exercício_26/Program.cs
@@ -0,0 +1,13 @@
+﻿//  Escreva um programa que exiba os números de 1 a 100 na tela em ordem decrescente.
+
+int num = 100;
+
+while(num > 0 )
+{
+    if(num == 0)
+    break;
+
+    Console.WriteLine(num);
+    num--;
+}
+
