7bfbe5f8727bc5e2fb75b5aebe43ef150bc20b94
LuanP.Castro <luan.castro@viannasempre.com.br>
2024-04-25 09:54:56 -0300

    Exercício 27 - Imprimindo o fatorial de um número com While

---
+++ Exercício_27/Program.cs
@@ -0,0 +1,19 @@
+﻿//  Escreva um programa que leia: - aquantidade de números que deverá processar: 
+// - os números que deverá processar,e calcule e exiba, para cada número a ser processado o seu fatorial. 
+// Lembrete: O fatorial de um número N é dado pela fórmula: N! = 1 * 2 * 3 * 4 * 5 * ... * N
+
+
+int numero, resultado;
+
+Console.WriteLine("insira o número que você deseja calcular o seu fatorial: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+resultado = 1;
+
+while(numero > 0)
+{
+    resultado = resultado * numero;
+    numero = numero - 1;
+}
+
+Console.WriteLine("O fatorial do número informado é: {0}", resultado);
