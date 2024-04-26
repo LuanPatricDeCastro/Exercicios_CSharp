f49dbb1ce203c991ae481856e8da0fa3e7975312
LuanP.Castro <luan.castro@viannasempre.com.br>
2024-04-26 05:14:29 -0300

    Exercício 28 - Imprimindo os números ímpares entre 100 e 200

---
+++ Exercicio_28/Program.cs
@@ -0,0 +1,11 @@
+﻿//  Faça um programa que gera e escreve os números ímpares dos números lidos entre 100 e 200. 
+
+int num = 100;
+
+while(num <= 200 )
+{
+    if(num % 2 != 0)
+
+    Console.WriteLine(num);
+    num++;
+}
