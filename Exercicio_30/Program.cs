1766c033b5dd9995519a4c58c6037cfadfda5086
LuanP.Castro <luan.castro@viannasempre.com.br>
2024-04-26 05:39:12 -0300

    Exercício 30 - Imprimindo uma tabuada

---
+++ Exercicio_30/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Faça um programa para imprimir uma tabuada.
+
+int numeroTabuada, multiplicador, valoresTabuada;
+
+multiplicador = 1;
+
+Console.Write("Insira o número que deseja imprimir a tabuada: ");
+numeroTabuada = Convert.ToInt32(Console.ReadLine());
+
+while(multiplicador < 11)
+{
+    
+    valoresTabuada = numeroTabuada * multiplicador;
+    Console.WriteLine("{0} x {1} = {2} ",numeroTabuada, multiplicador, valoresTabuada);
+    multiplicador++;
+
+}
