286feef00de94ac5134b77130304ab9e35c2ab12
LuanP.Castro <luan.castro@viannasempre.com.br>
2024-05-01 06:29:52 -0300

    Exercício 31 - Vetor que define numero par e ímpar e mostra a soma

---
+++ Exercicio_31/Program.cs
@@ -0,0 +1,27 @@
+﻿// Escrever um programa que leia um conjunto de números positivos, e exiba se o número lido é par ou ímpar. Exiba 
+// ao final a soma total dos números pares lidos e também a soma dos números ímpares lidos. Suporemos que o 
+// número de elementos deste conjunto não é conhecido, e que um número negativo será utilizado para sinalizar o 
+// fim dos dados.
+
+int i, numero, somaimpar = 0, somapar = 0;
+for(i = 0; i < 10; i++)
+{
+    Console.Write("Informe um número: ");
+    numero = Convert.ToInt32(Console.ReadLine());
+    if(numero < 0)
+    {
+        Console.WriteLine("Aplicação encerrada.");
+        break;
+    } else if(numero % 2 == 0)
+    {
+        Console.WriteLine("O número é par.");
+        somapar += numero;
+    } else 
+    {
+        Console.WriteLine("O número é ímpar.");
+        somaimpar+= numero;
+    }
+}
+
+Console.WriteLine("A soma dos números pares é: {0}", somapar);
+Console.WriteLine("A soma dos números pares é: {0}", somaimpar);
