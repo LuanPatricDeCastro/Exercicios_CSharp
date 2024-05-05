a9059300697987e82f50f06d4e7fcf1e182cb0e6
LuanP.Castro <luan.castro@viannasempre.com.br>
2024-05-05 07:29:49 -0300

    Exercício 35 - Programa que conte de 1 até 100 e exiba os múltiplos de 10

---
+++ Exercicio_35/Program.cs
@@ -0,0 +1,13 @@
+﻿/*  Faça um programa que conte de 1 até 100 e a cada múltiplo de 10 exiba uma mensagem: "Múltiplo de 10".*/ 
+
+int numero = 1;
+
+while(numero <= 100)
+{
+    Console.WriteLine(numero);
+    if(numero % 10 == 0) 
+    {
+        Console.WriteLine("Múltiplo de 10.");
+    }
+    numero++;
+}
